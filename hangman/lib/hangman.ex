7f723a8b0cbde6ca5e870828b284dd1f147832f4
brianwlego <legobw@gmail.com>
2021-01-20 22:27:50 -0500

    Fix _guess issue

--- hangman/lib/hangman.ex
+++ hangman/lib/hangman.ex
@@ -8,7 +8,7 @@ defmodule Hangman do
     GenServer.call(game_pid, {:make_move, guess})
   end
 
-  def tally(game_pid, _guess) do
+  def tally(game_pid) do
     GenServer.call(game_pid, {:tally})
   end
 end
